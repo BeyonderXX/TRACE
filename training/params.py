94934e6fc46a18158039b57ec2b2a49bc2722cd4
TeNko Tsukimi <45878717+cmnfriend@users.noreply.github.com>
2023-09-07 22:07:34 +0800

    add O-LoRA and LFPT5

--- training/params.py
+++ training/params.py
@@ -3,13 +3,17 @@ from model.Regular.LwF import LwF
 from model.Regular.EWC import EWC
 from model.Regular.GEM import GEM
 from model.Regular.OGD import OGD
+from model.Replay.LFPT5 import LFPT5
+from model.Regular.O_LoRA import O_LoRA
 
 
 Method2Class = {"PP":PP,
                 "EWC":EWC,
                 "GEM":GEM,
                 "OGD":OGD,
-                "LwF":LwF}
+                "LwF":LwF,
+                "LFPT5":LFPT5, 
+                "O-LoRA":O_LoRA}
 
 AllDatasetName = ["C-STANCE","FOMC","MeetingBank","Papyrus-f","Py150","ScienceQA","ToolBench","NumGLUE-cm","NumGLUE-ds"]
 
