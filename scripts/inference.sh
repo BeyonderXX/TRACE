b152c0e39968e17da46f5069886cfdd4b5a416c8
zhangyuansen <1589638971@qq.com>
2023-09-16 14:33:31 +0800

    update inference script

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -2,11 +2,13 @@
 port=$(shuf -i25000-30000 -n1)
 deepspeed --include=localhost:0,1,2,3,4,5,6,7 --master_port $port inference/infer_multi.py  \
     --data_path /mnt/data/user/zhang_yuansen/LLM-CL_Benchmark \
-    --dataset_name FOMC,C-STANCE,NumGLUE-cm,NumGLUE-ds,ScienceQA,MeetingBank,Py150,Papyrus-f \
-    --model_name_or_path /workspace/output/llama-7b-chat-seq-naive \
+    --dataset_name FOMC,C-STANCE \
+    --model_name_or_path /mnt/data/user/zhang_yuansen/PTMs/llama-2-7b \
+    --inference_model_path /workspace/output/llama-7b-chat-seq-cl/OGD \
     --inference_batch 16 \
     --max_prompt_len 2048 \
     --max_ans_len 512 \
     --seed 1234 \
     --deepspeed \
+    "--CL_method","OGD" \
     --inference_output_path /workspace/output/llama-7b-chat-inference
