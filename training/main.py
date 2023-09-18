92fce8ce8b0d8aeff863ff8b7d2c25b3400658fd
TeNko Tsukimi <45878717+cmnfriend@users.noreply.github.com>
2023-09-18 17:28:12 +0800

    fix bugs

--- training/main.py
+++ training/main.py
@@ -453,7 +453,6 @@ def main():
     if args.CL_method in Method2Class.keys():
         CL_Trainer = Method2Class[args.CL_method](model, tokenizer, optimizer, train_task_list, eval_task_list, test_task_list, args)
         CL_Trainer.train_continual()
-        CL_Trainer.save_model()
         
     else:
         #### Train ####
