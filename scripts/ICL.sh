a76c15e927173c5af2d22d9d44930f1f5535ddcc
Xiao Wang <xiao_wang20@fudan.edu.cn>
2023-09-21 10:34:59 +0800

    Add slurm ICL command

--- scripts/ICL.sh
+++ scripts/ICL.sh
@@ -11,3 +11,35 @@ deepspeed --include=localhost:0 --master_port $port inference/ICL.py  \
     --deepspeed \
     --demonstrations_num 6 \
     --inference_output_path /mnt/data/user/zhang_yuansen/outputs_LLM-CL/ICL > /mnt/data/user/zhang_yuansen/outputs_LLM-CL/ICL/infer.log 2>&1 &
+
+
+
+
+# for slurm
+srun --partition=xai --mpi=pmi2 --gres=gpu:8 -n1 --ntasks-per-node=1 --job-name=asb-llama --kill-on-bad-exit=1 /mnt/petrelfs/wangxiao/miniconda3/envs/cl/bin/deepspeed --master_port 51459 training/main.py  \
+    --data_path /mnt/petrelfs/wangxiao/DATA/LLM-CL-Benchmark/LLM-CL-Benchmark_5000 \
+    --dataset_name C-STANCE,FOMC,MeetingBank,Py150,ScienceQA,NumGLUE-cm,NumGLUE-ds,20Minuten \
+    --model_name_or_path /mnt/petrelfs/wangxiao/MODELS/llama2HF/7B-Chat \
+    --inference_batch 4 \
+    --max_prompt_len 3584 \
+    --max_ans_len 512 \
+    --seed 1234 \
+    --deepspeed \
+    --demonstrations_num 6 \
+    --inference_output_path /mnt/petrelfs/wangxiao/LLM-Continual-Learning/CKPT/ICL/llama2-7b > llama2_7b_ICL_infer.log 2>&1 &
+
+
+
+
+srun --partition=xai --mpi=pmi2 --gres=gpu:8 -n1 --ntasks-per-node=1 --job-name=asb-llama --kill-on-bad-exit=1 /mnt/petrelfs/wangxiao/miniconda3/envs/cl/bin/deepspeed --master_port 51459 training/main.py  \
+    --data_path /mnt/petrelfs/wangxiao/DATA/LLM-CL-Benchmark/LLM-CL-Benchmark_5000 \
+    --dataset_name C-STANCE,FOMC,MeetingBank,Py150,ScienceQA,NumGLUE-cm,NumGLUE-ds,20Minuten \
+    --model_name_or_path /mnt/petrelfs/wangxiao/MODELS/vicuna-7b-v1.5 \
+    --inference_batch 4 \
+    --max_prompt_len 3584 \
+    --max_ans_len 512 \
+    --seed 1234 \
+    --deepspeed \
+    --demonstrations_num 6 \
+    --inference_output_path /mnt/petrelfs/wangxiao/LLM-Continual-Learning/CKPT/ICL/vicuna-7b > vicuna-7b_ICL_infer.log 2>&1 &
+
