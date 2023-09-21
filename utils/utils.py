dec7fd3f2616e556875b25067060b98b6c9fcfc2
Xiao Wang <xiao_wang20@fudan.edu.cn>
2023-09-21 19:16:28 +0800

    Fix falcon tokenizer fix bug

--- utils/utils.py
+++ utils/utils.py
@@ -82,7 +82,7 @@ def save_hf_format(model, tokenizer, args, sub_folder=""):
     #         del save_dict[key]
     torch.save(save_dict, output_model_file)
     model_to_save.config.to_json_file(output_config_file)
-    tokenizer.save_vocabulary(output_dir)
+    tokenizer.save_pretrained(output_dir)
 
 def set_random_seed(seed):
     if seed is not None:
