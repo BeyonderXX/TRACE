0ec39bef2b09489df75932fda532b2c4ee7d5b67
zhangyuansen <1589638971@qq.com>
2023-09-21 19:26:32 +0800

    Update scripts

--- scripts/infer_seq.sh
+++ scripts/infer_seq.sh
@@ -2,7 +2,7 @@
 port=$(shuf -i25000-30000 -n1)
 deepspeed --include=localhost:0 --master_port $port inference/infer_single.py  \
     --data_path /mnt/data/user/zhang_yuansen/LLM-CL_Benchmark \
-    --dataset_name C-STANCE,FOMC,MeetingBank,Py150,ScienceQA,NumGLUE-cm,NumGLUE-ds,20Minuten \
+    --inference_tasks C-STANCE,FOMC,MeetingBank,Py150,ScienceQA,NumGLUE-cm,NumGLUE-ds,20Minuten \
     --model_name_or_path /mnt/data/user/zhang_yuansen/PTMs/llama-2-7b \
     --inference_model_path /mnt/data/user/zhang_yuansen/outputs_LLM-CL/naive \
     --inference_batch 4 \
